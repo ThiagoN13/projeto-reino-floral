e14984d2315dbed134b49c40440a3a9d5ad14d5e
Thiago <thiago.nogueira187@gmail.com>
2017-05-26 12:22:04 -0300

    Organizacao de pastas e correcoes de bugs adicionado bower

--- public/config/stateProvider.js
+++ public/config/stateProvider.js
@@ -94,4 +94,3 @@ angular.module('app')
         }
       });
   }]);
-  
---
+++ public/directives/ui-alert/ui-alert.js
@@ -0,0 +1,10 @@
+angular.module('app')
+    .directive('uiRequired', function () {
+    return {
+        restrict: 'AE',
+
+        link: function ($scope, element, attrs) {
+
+        }
+    }
+});
